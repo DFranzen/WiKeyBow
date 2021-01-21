312c672b636062954c0598cad099fdad54ee8bc7
Daniel Franzen <daniel.franzen.blubb@gmail.com>
2021-01-21 11:53:40 +0200

    Improved threading, Added layer support

--- WiKeyBow.py
+++ WiKeyBow.py
@@ -14,7 +14,46 @@ keyNames = ["key_1_in_row_1", "key_1_in_row_2","key_1_in_row_3","key_1_in_row_4"
 
 pins = [20,6,22,17,16,12,24,27,26,13,5,23]
 
-keys = {
+layer1 = {
+    "key_1_in_row_1": {
+        "name":  "Lamp"
+        "color": 0x1eff1e,
+        "colorOFF": 0xFF0000,
+        "colorON": 0x00FF00,
+        "state_req": {
+            "bash": "echo ON"
+            "url": "http://...",
+            "path": ["prop1", "prop2"],
+            "stateON": 'ON'
+        },
+        "keydown": {
+            "layer": 0,
+            "bash": "echo ON",
+            "bashON": "echo ON",
+            "bashOFF": "echo ON",
+            "url": "http://...",
+            "urlON": "http://...",
+            "urlOFF": "http://...",
+            "header": {"content-type": "application/json"},
+            "body": "{\"on\":false}",
+            "bodyON": "{\"on\":false}",
+            "bodyOFF": "{\"on\":true}"
+        }
+    },
+    "key_2_in_row_1": {},
+    "key_3_in_row_1": {},
+    "key_1_in_row_2": {},
+    "key_2_in_row_2": {},
+    "key_3_in_row_2": {},
+    "key_1_in_row_3": {},
+    "key_2_in_row_3": {},
+    "key_3_in_row_3": {},
+    "key_1_in_row_4": {},
+    "key_2_in_row_4": {},
+    "key_3_in_row_4": {}
+}
+
+layer2 = {
     "key_1_in_row_1": {
         "name":  "Lamp"
         "color": 0x1eff1e,
@@ -51,22 +90,53 @@ keys = {
     "key_2_in_row_4": {},
     "key_3_in_row_4": {}
 }
+
+layers = [{}, layer1, layer2]
+layer_select = 1
+selection_layer = {}
+
+for i in range(1,len(layers)):
+    column = int((i-1)/4) + 1
+    row = 4 - int((i-1)%4)
+    keyName = "key_" + str(column) + "_in_row_" + str(row)
+    layer = layers[i]
+    selection_layer[keyName] = {
+        "color": layer["color"],
+        "keydown": {
+            "layer": i
+        }
+            
+    }
+layers[0] = selection_layer
+
 strip = apa102.APA102(num_led=12, mosi=10, sclk=11, order='rgb')
 
-def update_color(key_name, color):
+def set_color(key_name, color):
     index = keyNames.index(key_name)
     strip.set_pixel_rgb(index, color)
     strip.show()
 
 def get_color(key_name):
+    global layer_select
+    keys = layers[layer_select]
+    if not key_name in keys:
+        return 0x000000
+    
     key = keys[key_name]
     state = get_state(key)
+
     
     if "color" + state in key:
         return key["color" + state]
     elif "color" in key:
         return key["color"]
     return 0x000000
+
+def update_color(key_name):
+    l_s = layer_select
+    color = get_color(key_name)
+    if (l_s == layer_select):
+        set_color(key_name,color)
     
 def get_keydown_url(key):
     state = get_state(key)
@@ -104,18 +174,24 @@ def get_keydown_bash(key):
 
     return ""
 
+def update_all():
+    print ("Starting update")
+    keys = layers[layer_select]
+
+    for kN in keyNames:
+        if kN in keys:
+            invalidate_state(keys[kN])
+            thread =threading.Thread(target=update_color, name="Color " + kN, args=[kN])
+            thread.start()
+        else:
+            set_color(kN,0x000000)
+    print("update finished")
+        
 
 def update_state():
-    # threads = {}
+    global layer_select
     while True:
-        print ("Starting update")
-        for kN in keyNames:
-            invalidate_state(keys[kN])
-            color = get_color(kN)
-            update_color(kN,color)
-#            threads[kN] =threading.Thread(target=update_color, name="Color " + kN, args=[kN,color])
-#            threads[kN].start()
-        print("update finished")
+        update_all()
         sleep(2)
 
 def invalidate_state(key):
@@ -149,9 +225,12 @@ def get_state(key):
             try:
                 process = subprocess.Popen(state_req["bash"], shell=True, stdout=subprocess.PIPE)
                 output, error = process.communicate()
+                
                 res = output.decode('UTF-8').rstrip()
                 print("State result is "+ res)
-                if (res == state_req["stateON"]):
+                if process.returncode != 0:
+                    key["state"]=""
+                elif (res == state_req["stateON"]):
                     key["state"]="ON"
                 else:
                     key["state"]="OFF"
@@ -163,54 +242,80 @@ def get_state(key):
         
     return key["state"]
 
+key_down = {}
+for kN in keyNames:
+    key_down[kN]=False
+
+def handle_keydown(keyName):
+    global layer_select
+
+    print("Handling keydown on " + keyName)
+    
+    keys = layers[layer_select]
+    key = keys[keyName]
         
+    #handle keydown event
+    if "keydown" in key:
+        keydown = key["keydown"]
+        url = get_keydown_url(key)
+        bash = get_keydown_bash(key)
+        if not (url == ""):
+            body = get_keydown_body(key)
+            # execute correct body
+            try: 
+                res = requests.put(url,data=body, headers=keydown["header"])
+            except:
+                res = ""
+        if not (bash == ""):
+            try:
+                process = subprocess.Popen(bash, shell=True, stdout=subprocess.PIPE)
+                output, error = process.communicate()
+
+            except:
+                output = ""
+        if "layer" in keydown:
+            layer_select = keydown["layer"]
+            print("switching to layer " + str(layer_select))
+            update_all()
+        invalidate_state(key)
+        sleep(0.3)
+    update_color(keyName)
+    print("finished Handling keydown on " + keyName)
+
+    
 def handle(button):
+    global key_down
     keyName = keyNames[button]
+    keys = layers[layer_select]
+    print("event on " + keyName)
+
+    print("state is " + str(GPIO.input(pins[button])))
+    
+    if GPIO.input(pins[button]) == 1:
+        # KeyUp Event
+        key_down[keyName]=False
+        print("is keyup Event")
+        return
+
+    if key_down[keyName]:
+        # if already down -> ignore
+        print("Bounce detected")
+        return
+    else: 
+        key_down[keyName] = True
+
     if keyName in keys:
-        key = keys[keyName]
-
-        # handle non-keydown events
-        if not "down" in key:
-            key["down"] = False
-        if GPIO.input(pins[button]) == 1:
-            #update_state()
-            key["down"]=False
-            return
-        if key["down"]:
-            return
-        key["down"] = True
-
-        #handle keydown event
-        if "keydown" in key:
-            keydown = key["keydown"]
-            url = get_keydown_url(key)
-            bash = get_keydown_bash(key)
-            if not (url == ""):
-                body = get_keydown_body(key)
-                # execute correct body
-                try: 
-                    res = requests.put(url,data=body, headers=keydown["header"])
-                except:
-                    res = ""
-            elif not (bash == ""):
-                try:
-                    process = subprocess.Popen(bash, shell=True, stdout=subprocess.PIPE)
-                    output, error = process.communicate()
-                except:
-                    output = ""
-            invalidate_state(key)
-    sleep(0.3)
-    update_color(keyName, get_color(keyName))
-    print("handle done")
-#    update_state()
-
-
+        handler = threading.Thread(target=handle_keydown, args=[keyName])
+        handler.start() 
+        
+    print("Event delegated")
 
 def set_handler(id):
     pin = pins[id]
     
     GPIO.setup(pin,GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    GPIO.add_event_detect(pin, GPIO.BOTH, bouncetime=300, callback=lambda x: handle(id))
+    GPIO.add_event_detect(pin, GPIO.BOTH, bouncetime=50, callback=lambda x: handle(id))
+
 
 t_handlers = {}
 for i in range(0,len(pins)):
